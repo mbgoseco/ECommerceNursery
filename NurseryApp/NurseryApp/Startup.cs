6eb2aadce5dc1cff96f1d7e5b05cd49429c3a0f7
Clarice Costello <claricostello@outlook.com>
2019-02-19 09:34:50 -0800

    Added DbContext connection to startup

--- NurseryApp/NurseryApp/Startup.cs
+++ NurseryApp/NurseryApp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace NurseryApp
@@ -15,6 +16,8 @@ namespace NurseryApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<NurseryDbContext>(options =>
+                options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]))
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
