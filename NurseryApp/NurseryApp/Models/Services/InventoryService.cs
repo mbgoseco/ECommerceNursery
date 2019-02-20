ef23d18dbf51145abfc2a3977bfdc8a2c3cd21f9
Clarice Costello <claricostello@outlook.com>
2019-02-20 10:24:54 -0800

    Injected NurseryDbContext into Inventory Service

--- NurseryApp/NurseryApp/Models/Services/InventoryService.cs
+++ NurseryApp/NurseryApp/Models/Services/InventoryService.cs
@@ -1,4 +1,5 @@
-﻿using NurseryApp.Models.Interfaces;
+﻿using NurseryApp.Data;
+using NurseryApp.Models.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,12 @@ namespace NurseryApp.Models.Services
 {
     public class InventoryService : IInventory
     {
+        private readonly NurseryDbContext _context;
+
+        public InventoryService(NurseryDbContext context)
+        {
+            _context = context;
+        }
         public Task CreateProduct()
         {
             throw new NotImplementedException();
