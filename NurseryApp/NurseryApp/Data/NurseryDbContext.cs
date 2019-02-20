7db90a69089f0dd00845f438394b9fb2b7fe4f81
Clarice Costello <claricostello@outlook.com>
2019-02-20 09:37:04 -0800

    Added 10 seeded Products

--- NurseryApp/NurseryApp/Data/NurseryDbContext.cs
+++ NurseryApp/NurseryApp/Data/NurseryDbContext.cs
@@ -16,9 +16,70 @@ namespace NurseryApp.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
-            public DbSet<Product> Products { get; set; }
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ID = 1,
+                    Name = "Ranier Cherry Tree",
+                    Type = Product.PlantType.Tree
+                },
+                new Product
+                {
+                    ID = 2,
+                    Name = "Yoshino Cherry Tree",
+                    Type = Product.PlantType.Tree
+                },
+                new Product
+                {
+                    ID = 3,
+                    Name = "Rhododendron",
+                    Type = Product.PlantType.Shrub
+                },
+                new Product
+                {
+                    ID = 4,
+                    Name = "Dwarf Apple Tree",
+                    Type = Product.PlantType.Tree
+                },
+                new Product
+                {
+                    ID = 5,
+                    Name = "Dwarf Plum Tree",
+                    Type = Product.PlantType.Tree
+                },
+                new Product
+                {
+                    ID = 6,
+                    Name = "Red Raspberry",
+                    Type = Product.PlantType.Shrub
+                },
+                new Product
+                {
+                    ID = 7,
+                    Name = "Echivera",
+                    Type = Product.PlantType.Houseplant
+                },
+                new Product
+                {
+                    ID = 8,
+                    Name = "Magestic Palm",
+                    Type = Product.PlantType.Houseplant
+                },
+                new Product
+                {
+                    ID = 9,
+                    Name = "California Black Oak",
+                    Type = Product.PlantType.Tree,
+                },
+                new Product
+                {
+                    ID = 10,
+                    Name = "Giant Sunflower",
+                    Type = Product.PlantType.Flower
+                }
+                );
         }
+        public DbSet<Product> Products { get; set; }
 
     }
 }
