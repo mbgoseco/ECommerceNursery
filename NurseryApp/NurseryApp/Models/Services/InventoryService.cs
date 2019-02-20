651ab1512fc5979044da60bbee11bacd51e0b416
Clarice Costello <claricostello@outlook.com>
2019-02-20 10:23:16 -0800

    Implamented IInventory in InventoryService

--- NurseryApp/NurseryApp/Models/Services/InventoryService.cs
+++ NurseryApp/NurseryApp/Models/Services/InventoryService.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using NurseryApp.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace NurseryApp.Models.Services
 {
-    public class InventoryService
+    public class InventoryService : IInventory
     {
+        public Task CreateProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductByID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
