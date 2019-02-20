f55fb4a7a61fd7f8223ef7f5369300ed6c5aeb7f
Clarice Costello <claricostello@outlook.com>
2019-02-20 09:42:46 -0800

    Added new Properties to seeded data

--- NurseryApp/NurseryApp/Data/NurseryDbContext.cs
+++ NurseryApp/NurseryApp/Data/NurseryDbContext.cs
@@ -21,61 +21,102 @@ namespace NurseryApp.Data
                 {
                     ID = 1,
                     Name = "Ranier Cherry Tree",
-                    Type = Product.PlantType.Tree
+                    Type = Product.PlantType.Tree,
+                    Sku = "abcdef",
+                    Description = "Placeholder Description",
+                    Price = 1.00m,
+                    Img = "https://images.pexels.com/photos/1002703/pexels-photo-1002703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+
                 },
                 new Product
                 {
                     ID = 2,
                     Name = "Yoshino Cherry Tree",
-                    Type = Product.PlantType.Tree
+                    Type = Product.PlantType.Tree,
+                    Sku = "qwerty",
+                    Description = "Placeholder Description",
+                    Price = 1.00m,
+                    Img = "https://images.pexels.com/photos/1002703/pexels-photo-1002703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                 },
                 new Product
                 {
                     ID = 3,
                     Name = "Rhododendron",
-                    Type = Product.PlantType.Shrub
+                    Type = Product.PlantType.Shrub,
+                    Sku = "123345",
+                    Description = "Placeholder Description",
+                    Price = 1.00m,
+                    Img = "https://images.pexels.com/photos/1002703/pexels-photo-1002703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                 },
                 new Product
                 {
                     ID = 4,
                     Name = "Dwarf Apple Tree",
-                    Type = Product.PlantType.Tree
+                    Type = Product.PlantType.Tree,
+                    Sku = "yuiop",
+                    Description = "Placeholder Description",
+                    Price = 1.00m,
+                    Img = "https://images.pexels.com/photos/1002703/pexels-photo-1002703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                 },
                 new Product
                 {
                     ID = 5,
                     Name = "Dwarf Plum Tree",
-                    Type = Product.PlantType.Tree
+                    Type = Product.PlantType.Tree,
+                    Sku = "567ghj",
+                    Description = "Placeholder Description",
+                    Price = 1.00m,
+                    Img = "https://images.pexels.com/photos/1002703/pexels-photo-1002703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                 },
                 new Product
                 {
                     ID = 6,
                     Name = "Red Raspberry",
-                    Type = Product.PlantType.Shrub
+                    Type = Product.PlantType.Shrub,
+                    Sku = "098lkjh",
+                    Description = "Placeholder Description",
+                    Price = 1.00m,
+                    Img = "https://images.pexels.com/photos/1002703/pexels-photo-1002703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                 },
                 new Product
                 {
                     ID = 7,
                     Name = "Echivera",
-                    Type = Product.PlantType.Houseplant
+                    Type = Product.PlantType.Houseplant,
+                    Sku = "zxcv34",
+                    Description = "Placeholder Description",
+                    Price = 1.00m,
+                    Img = "https://images.pexels.com/photos/1002703/pexels-photo-1002703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                 },
                 new Product
                 {
                     ID = 8,
                     Name = "Magestic Palm",
-                    Type = Product.PlantType.Houseplant
+                    Type = Product.PlantType.Houseplant,
+                    Sku = "9d9emd",
+                    Description = "Placeholder Description",
+                    Price = 1.00m,
+                    Img = "https://images.pexels.com/photos/1002703/pexels-photo-1002703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                 },
                 new Product
                 {
                     ID = 9,
                     Name = "California Black Oak",
                     Type = Product.PlantType.Tree,
+                    Sku = "098asdf",
+                    Description = "Placeholder Description",
+                    Price = 1.00m,
+                    Img = "https://images.pexels.com/photos/1002703/pexels-photo-1002703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                 },
                 new Product
                 {
                     ID = 10,
                     Name = "Giant Sunflower",
-                    Type = Product.PlantType.Flower
+                    Type = Product.PlantType.Flower,
+                    Sku = "abci876k",
+                    Description = "Placeholder Description",
+                    Price = 1.00m,
+                    Img = "https://images.pexels.com/photos/1002703/pexels-photo-1002703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                 }
                 );
         }
