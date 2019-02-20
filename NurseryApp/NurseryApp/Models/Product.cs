6a697232670eaf942a6c99e32eb6eda6a8ec4533
Clarice Costello <claricostello@outlook.com>
2019-02-20 09:38:24 -0800

    Added more properties to Product Model

--- NurseryApp/NurseryApp/Models/Product.cs
+++ NurseryApp/NurseryApp/Models/Product.cs
@@ -11,6 +11,10 @@ namespace NurseryApp.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public PlantType Type { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Img { get; set; }
+        public string Sku { get; set; }
 
         public enum PlantType { Tree, Shrub, Flower, Houseplant };
     }
