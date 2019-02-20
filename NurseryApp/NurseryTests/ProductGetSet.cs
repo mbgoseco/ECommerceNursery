cd8cd066873229b1367e02fe2b2468b608620ebf
Clarice Costello <claricostello@outlook.com>
2019-02-20 10:13:19 -0800

    wrote setter tests

--- NurseryApp/NurseryTests/ProductGetSet.cs
+++ NurseryApp/NurseryTests/ProductGetSet.cs
@@ -84,5 +84,87 @@ namespace NurseryTests
             Assert.Equal("Img.Path", product.Img);
         }
 
+        [Fact]
+        public void CanSetID()
+        {
+            Product product = new Product
+            {
+                ID = 1
+            };
+
+            Assert.Equal(1, product.ID);
+        }
+
+
+        [Fact]
+        public void CanSetName()
+        {
+            Product product = new Product
+            {
+                Name = "SunFlower"
+            };
+            product.Name = "Apple Tree";
+
+            Assert.Equal("Apple Tree", product.Name);
+        }
+
+        [Fact]
+        public void CanSetType()
+        {
+            Product product = new Product
+            {
+                Type = Product.PlantType.Flower
+            };
+            product.Type = Product.PlantType.Houseplant;
+
+            Assert.Equal(Product.PlantType.Houseplant, product.Type);
+        }
+
+        [Fact]
+        public void CanSetSku()
+        {
+            Product product = new Product
+            {
+                Sku = "12345"
+            };
+            product.Sku = "abcde";
+
+            Assert.Equal("abcde", product.Sku);
+        }
+
+        [Fact]
+        public void CanSetDescription()
+        {
+            Product product = new Product
+            {
+                Description = "A Description"
+            };
+            product.Description = "Different Description";
+
+            Assert.Equal("Different Description", product.Description);
+        }
+
+        [Fact]
+        public void CanSetPrice()
+        {
+            Product product = new Product
+            {
+                Price = 100
+            };
+            product.Price = 200;
+
+            Assert.Equal(200, product.Price);
+        }
+
+        [Fact]
+        public void CanSetImg()
+        {
+            Product product = new Product
+            {
+                Img = "Img.Path"
+            };
+            product.Img = "new.Path";
+            Assert.Equal("new.Path", product.Img);
+        }
     }
 }
