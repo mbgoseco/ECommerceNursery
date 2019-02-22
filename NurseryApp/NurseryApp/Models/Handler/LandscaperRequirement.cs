9cc1427c707114acca147c91c102b031c385d81d
Clarice Costello <claricostello@outlook.com>
2019-02-22 09:44:47 -0800

    scaffolded handler

---
+++ NurseryApp/NurseryApp/Models/Handler/LandscaperRequirement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NurseryApp.Models.Handler
+{
+    public class LandscaperRequirement
+    {
+    }
+}
