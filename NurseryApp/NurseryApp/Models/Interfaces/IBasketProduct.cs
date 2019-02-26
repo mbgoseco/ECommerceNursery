9b1ac0da7ea0ca2eaea57c35ee2bc912dfc942fe
Clarice Costello <claricostello@outlook.com>
2019-02-26 10:39:02 -0800

    built out basket product interface

---
+++ NurseryApp/NurseryApp/Models/Interfaces/IBasketProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NurseryApp.Models.Interfaces
+{
+    interface IBasketProduct
+    {
+        Task AddBasketProduct(BasketProduct basketProduct);
+
+        Task<List<BasketProduct>> GetBasket();
+
+        Task<BasketProduct> GetBasketProductByID(int id);
+
+        Task UpdateQuantity(Product basketProduct);
+
+        Task DeleteBasketProductByID(int id);
+    }
+}
