2eb1992ceec9b180dac310e71d2561bd27e482dd
Clarice Costello <claricostello@outlook.com>
2019-02-26 20:37:12 -0800

    Built out Basket View Component

--- NurseryApp/NurseryApp/Models/Components/Basket.cs
+++ NurseryApp/NurseryApp/Models/Components/Basket.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NurseryApp.Models.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,19 @@ namespace NurseryApp.Models.Components
 {
     public class Basket : ViewComponent
     {
+        private readonly IBasketProduct _context;
+
+        public Basket(IBasketProduct context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string userID = UserClaimsPrincipal.Claims.First(name => name.Type == "id").Value;
+
+            var products = await _context.GetBasket(userID);
+            return View(products);
+        }
     }
 }
