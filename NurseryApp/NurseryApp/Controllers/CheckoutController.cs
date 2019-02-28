6acfc0e1abc9fff5bacbdddc72e5071c46fb933c
Clarice Costello <claricostello@outlook.com>
2019-02-28 09:45:24 -0800

    Started checkout controller set up

---
+++ NurseryApp/NurseryApp/Controllers/CheckoutController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NurseryApp.Models;
+using NurseryApp.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NurseryApp.Controllers
+{
+    public class CheckoutController
+    {
+        private readonly ICheckout _context;
+        private readonly IBasket _basketcontext;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CheckoutController(UserManager<ApplicationUser> userManager, ICheckout context)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+    }
+}
