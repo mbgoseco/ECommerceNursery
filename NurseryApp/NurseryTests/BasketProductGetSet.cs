5bb4710eecfdbf0c87f894b5ccd269073d615f1a
Clarice Costello <claricostello@outlook.com>
2019-03-09 10:12:28 -0800

    Wrote basket product getters and setters tests

--- NurseryApp/NurseryTests/BasketProductGetSet.cs
+++ NurseryApp/NurseryTests/BasketProductGetSet.cs
@@ -6,6 +6,63 @@ namespace NurseryTests
 {
     public class BasketProductGetSet
     {
-        
+        [Fact]
+        public void CanGetBasketID()
+        {
+            BasketProduct basketProduct = new BasketProduct()
+            {
+                BasketID = 1
+            };
+            Assert.Equal(1, basketProduct.BasketID);
+        }
+        [Fact]
+        public void CanGetProductID()
+        {
+            BasketProduct basketProduct = new BasketProduct()
+            {
+                ProductID = 1
+            };
+            Assert.Equal(1, basketProduct.ProductID);
+        }
+        [Fact]
+        public void CanGetQuantity()
+        {
+            BasketProduct basketProduct = new BasketProduct()
+            {
+                Quantity = 1
+            };
+            Assert.Equal(1, basketProduct.Quantity);
+        }
+
+        [Fact]
+        public void CanSetBasketID()
+        {
+            BasketProduct basketProduct = new BasketProduct()
+            {
+                BasketID = 1
+            };
+            basketProduct.BasketID = 2;
+            Assert.Equal(2, basketProduct.BasketID);
+        }
+        [Fact]
+        public void CanSetProductID()
+        {
+            BasketProduct basketProduct = new BasketProduct()
+            {
+                ProductID = 1
+            };
+            basketProduct.ProductID = 2;
+            Assert.Equal(2, basketProduct.ProductID);
+        }
+        [Fact]
+        public void CanSetQuantity()
+        {
+            BasketProduct basketProduct = new BasketProduct()
+            {
+                Quantity = 1
+            };
+            basketProduct.Quantity = 2;
+            Assert.Equal(2, basketProduct.Quantity);
+        }
     }
 }
