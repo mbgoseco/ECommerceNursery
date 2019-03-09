b80c8d7908544e3ad1d1697bfaec9e41120c1e88
Clarice Costello <claricostello@outlook.com>
2019-03-09 10:10:40 -0800

    wrote getters and setters for checkout product

--- NurseryApp/NurseryTests/CheckoutProductGetSet.cs
+++ NurseryApp/NurseryTests/CheckoutProductGetSet.cs
@@ -6,6 +6,63 @@ namespace NurseryTests
 {
     public class CheckoutProductGetSet
     {
-        
+        [Fact]
+        public void CanGetCheckoutID()
+        {
+            CheckoutProduct checkoutProduct = new CheckoutProduct()
+            {
+                CheckoutID = 1
+            };
+            Assert.Equal(1, checkoutProduct.CheckoutID);
+        }
+        [Fact]
+        public void CanGetProductID()
+        {
+            CheckoutProduct checkoutProduct = new CheckoutProduct()
+            {
+                ProductID = 1
+            };
+            Assert.Equal(1, checkoutProduct.ProductID);
+        }
+        [Fact]
+        public void CanGetQuantity()
+        {
+            CheckoutProduct checkoutProduct = new CheckoutProduct()
+            {
+                Quantity = 1
+            };
+            Assert.Equal(1, checkoutProduct.Quantity);
+        }
+
+        [Fact]
+        public void CanSetCheckoutID()
+        {
+            CheckoutProduct checkoutProduct = new CheckoutProduct()
+            {
+                CheckoutID = 1
+            };
+            checkoutProduct.CheckoutID = 2;
+            Assert.Equal(2, checkoutProduct.CheckoutID);
+        }
+        [Fact]
+        public void CanSetProductID()
+        {
+            CheckoutProduct checkoutProduct = new CheckoutProduct()
+            {
+                ProductID = 1
+            };
+            checkoutProduct.ProductID = 2;
+            Assert.Equal(2, checkoutProduct.ProductID);
+        }
+        [Fact]
+        public void CanSetQuantity()
+        {
+            CheckoutProduct checkoutProduct = new CheckoutProduct()
+            {
+                Quantity = 1
+            };
+            checkoutProduct.Quantity = 2;
+            Assert.Equal(2, checkoutProduct.Quantity);
+        }
     }
 }
