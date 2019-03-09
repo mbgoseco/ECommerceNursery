9042c90bbfe7a3a7442342263fbce1e1b78ebded
Clarice Costello <claricostello@outlook.com>
2019-03-09 10:46:19 -0800

    wrote basket model getters and setters tests

--- NurseryApp/NurseryTests/BasketGetSet.cs
+++ NurseryApp/NurseryTests/BasketGetSet.cs
@@ -6,6 +6,43 @@ namespace NurseryTests
 {
     public class BasketGetSet
     {
-        
+        [Fact]
+        public void CanGetID()
+        {
+            Basket basket = new Basket()
+            {
+                ID = 1
+            };
+            Assert.Equal(1, basket.ID);
+        }
+        [Fact]
+        public void CanGetUserID()
+        {
+            Basket basket = new Basket()
+            {
+                UserID = "string"
+            };
+            Assert.Equal("string", basket.UserID);
+        }
+        [Fact]
+        public void CanSetID()
+        {
+            Basket basket = new Basket()
+            {
+                ID = 1
+            };
+            basket.ID = 2;
+            Assert.Equal(2, basket.ID);
+        }
+        [Fact]
+        public void CanSetUserID()
+        {
+            Basket basket = new Basket()
+            {
+                UserID = "string"
+            };
+            basket.UserID = "new string";
+            Assert.Equal("new string", basket.UserID);
+        }
     }
 }
